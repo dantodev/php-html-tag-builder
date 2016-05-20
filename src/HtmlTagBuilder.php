58110119e905031a0125a0e285bf2a8d2aff1880
daniel <kontakt@daniel-kahl.de>
2016-05-20 16:37:22 +0200

    * add PSR-2 compatibility #3

--- src/HtmlTagBuilder.php
+++ src/HtmlTagBuilder.php
@@ -4,69 +4,69 @@ use Dtkahl\ArrayTools\Map;
 
 class HtmlTagBuilder
 {
-  private $_type        = "";
-  private $_text        = "";
-  public $attributes;
-  public $options;
+    private $_type = "";
+    private $_text = "";
+    public $attributes;
+    public $options;
 
-  /**
-   * HtmlTag constructor.
-   * @param string $type
-   * @param Map|array $attributes
-   * @param string $text
-   * @param array $options
-   */
-  public function __construct($type, $attributes = [], $text = "", array $options = [])
-  {
-    $this->_type       = (string) $type;
-    $this->_text       = (string) $text;
-    $this->attributes  = $attributes instanceof Map ? $attributes : new Map($attributes);
-    $this->options     = $options instanceof Map ? $options : new Map($options);
-  }
-
-  /**
-   * @return string
-   */
-  public function __toString()
-  {
-    return $this->render();
-  }
-
-  /**
-   * @return string
-   */
-  public function render()
-  {
-    $text       = $this->options->get('escape_text', true) ? htmlentities($this->_text) : $this->_text;
-    $attributes = "";
-
-    foreach ($this->attributes->toArray() as $name => $value) {
-      $attributes .= sprintf(' %s="%s"', $name, $value);
+    /**
+     * HtmlTag constructor.
+     * @param string $type
+     * @param Map|array $attributes
+     * @param string $text
+     * @param array $options
+     */
+    public function __construct($type, $attributes = [], $text = "", array $options = [])
+    {
+        $this->_type = (string)$type;
+        $this->_text = (string)$text;
+        $this->attributes = $attributes instanceof Map ? $attributes : new Map($attributes);
+        $this->options = $options instanceof Map ? $options : new Map($options);
     }
 
-    if (in_array($this->_type, ['meta', 'img', 'input', 'br', 'hr', 'link'])) {
-      return sprintf('<%s%s>', $this->_type, $attributes);
-    } else {
-      return sprintf('<%s%s>%s</%s>', $this->_type, $attributes, $text, $this->_type);
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->render();
     }
-  }
 
-  /**
-   * @param $text
-   * @return $this
-   */
-  public function setText($text)
-  {
-    $this->_text = $text;
-    return $this;
-  }
+    /**
+     * @return string
+     */
+    public function render()
+    {
+        $text = $this->options->get('escape_text', true) ? htmlentities($this->_text) : $this->_text;
+        $attributes = "";
 
-  /**
-   * @return string
-   */
-  public function getText()
-  {
-    return $this->_text;
-  }
+        foreach ($this->attributes->toArray() as $name => $value) {
+            $attributes .= sprintf(' %s="%s"', $name, $value);
+        }
+
+        if (in_array($this->_type, ['meta', 'img', 'input', 'br', 'hr', 'link'])) {
+            return sprintf('<%s%s>', $this->_type, $attributes);
+        } else {
+            return sprintf('<%s%s>%s</%s>', $this->_type, $attributes, $text, $this->_type);
+        }
+    }
+
+    /**
+     * @param $text
+     * @return $this
+     */
+    public function setText($text)
+    {
+        $this->_text = $text;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getText()
+    {
+        return $this->_text;
+    }
 
 }
--- tests/HtmlTagBuilderTest.php
+++ tests/HtmlTagBuilderTest.php
@@ -1,19 +1,20 @@
 <?php namespace Dtkahl\HtmlTagBuilder;
 
-class HtmlTagBuilderTest extends \PHPUnit_Framework_TestCase {
+class HtmlTagBuilderTest extends \PHPUnit_Framework_TestCase
+{
 
-  public function testConstructor()
-  {
-    $tag = new HtmlTagBuilder('div', ['class' => 'foo'], 'bar');
-    $this->assertEquals('<div class="foo">bar</div>', $tag->render());
-  }
+    public function testConstructor()
+    {
+        $tag = new HtmlTagBuilder('div', ['class' => 'foo'], 'bar');
+        $this->assertEquals('<div class="foo">bar</div>', $tag->render());
+    }
 
-  public function testBuild()
-  {
-    $tag = new HtmlTagBuilder('div');
-    $tag->attributes->set('class', 'foo');
-    $tag->setText('bar');
-    $this->assertEquals('<div class="foo">bar</div>', $tag->render());
-  }
+    public function testBuild()
+    {
+        $tag = new HtmlTagBuilder('div');
+        $tag->attributes->set('class', 'foo');
+        $tag->setText('bar');
+        $this->assertEquals('<div class="foo">bar</div>', $tag->render());
+    }
 
 }
